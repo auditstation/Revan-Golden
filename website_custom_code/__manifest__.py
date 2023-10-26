fde312b72704b689d9070dd8ec7192e16459e5ec
auditstation <140529980+auditstation@users.noreply.github.com>
2023-10-26 16:53:58 +0400

    [IMP] website

--- website_custom_code/__manifest__.py
+++ website_custom_code/__manifest__.py
@@ -1,27 +1,40 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Website Custom Code',
-    'category': 'Website',
+    'name': "Website Custom Code",
+
+    'summary': """
+        Show flag on currency Hide Unavailable Variants on the website""",
+    'description': """ This module gives option to hide the variants form the website by simply enabling a
+    boolean field. Here no need to set the exclusions. So customer can only see the valid and available attribute 
+    values for the product.
+    Hide Variants,
+    Hide Attribute Values,
+    Odoo Hide Variants,
+    Hide Product Variants """,
+
     'sequence': 50,
-    'version': '16.0.1.0.0',
-    'description': """Website Custom Code""",
-    'Summary': """Website Custom Code""",
     'author': "Mindrich Technologies Pvt. Ltd.",
     'company': 'Mindrich Technologies Pvt. Ltd.',
-    'maintainer': 'Hassan',
-    'website': "",
-    'depends': ['website', 'website_sale'],
+    'category': 'eCommerce',
+    'version': '1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website','website_sale'],
+
+    # always loaded
     'data': [
-        'views/website_sale_inherit.xml',
-        'views/res_currency_view.xml',
+        'views/product_view.xml',
+        'views/website_sale_templates.xml',
         'views/price_list_template.xml',
+        'views/res_currency_view.xml',
     ],
     'assets': {
         'web.assets_frontend': [
-            'website_custom_code/static/src/scss/main.scss',
-        ],
+            '/website_custom_code/static/src/scss/main.css',
+            '/website_custom_code/static/src/js/hide.js',
+        ]
     },
-    'license': 'LGPL-3',
+    # only loaded in demonstration mode
     'installable': True,
     'application': True,
-    'auto_install': False,
 }
