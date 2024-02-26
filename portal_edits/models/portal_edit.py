fca9ae1e4613d12d9d7adc98a84bdb71bea1fd3e
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-02-26 10:26:04 +0300

    check data

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -16,8 +16,8 @@ import logging
 
 _logger = logging.getLogger(__name__)
 class PortalInherit(CustomerPortal):
-    MANDATORY_BILLING_FIELDS = ["name", "phone","state_id","country_id"]
-    OPTIONAL_BILLING_FIELDS = ["zipcode","city","street","email","vat", "company_name"]
+    MANDATORY_BILLING_FIELDS = ["name", "phone","state_id","country_id","street"]
+    OPTIONAL_BILLING_FIELDS = ["zipcode","city","email","vat", "company_name"]
 
 class WebsitePortalInherit(WebsiteSale):
     def _get_mandatory_fields_billing(self, country_id=False):
