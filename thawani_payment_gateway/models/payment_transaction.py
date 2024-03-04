a95012d40dadef88841ee77dcb6ec860515b6796
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-03-04 13:08:30 +0300

    after_done

--- thawani_payment_gateway/models/payment_transaction.py
+++ thawani_payment_gateway/models/payment_transaction.py
@@ -226,7 +226,7 @@ class PaymentTransaction(models.Model):
                     _logger.info(payment_status)
                     if payment_status == 'paid':
                         self.sudo()._set_done()
-                        _reconcile_after_done
+                        # _reconcile_after_done
                         # self.with_user(SUPERUSER_ID)._check_amount_and_confirm_order()
                         # self._log_message_on_linked_documents
                         # self._send_order_confirmation_mail()
