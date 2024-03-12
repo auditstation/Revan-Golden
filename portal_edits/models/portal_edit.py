75646c4536e3b0df758556037ed53046abd2ff1d
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-03-12 12:58:37 +0300

    log

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -66,7 +66,56 @@ class WebsitePortalsInherit(WebsiteSale):
         return {
             k: (bool(v) and int(v)) if k in partner_fields and partner_fields[k].type == 'many2one' else v
             for k, v in values.items()
-        }    
+        }   
+
+    def _create_or_edit_partner(self, partner_details, edit=False, **custom_values):
+        """ Create or update a partner
+
+        To create a partner, this controller usually calls `values_preprocess()`, then
+        `checkout_form_validate()`, then `values_postprocess()` and finally `_checkout_form_save()`.
+        Since these methods are very specific to the checkout form, this method makes it possible to
+        create  a partner for more specific flows like express payment, which does not require all
+        the checks carried out by the previous methods. Parts of code in this method come from those.
+
+        :param dict partner_details: The values needed to create the partner or to edit the partner.
+        :param bool edit: Whether edit an existing partner or create one, defaults to False.
+        :param dict custom_values: Optional custom values for the creation or edition.
+        :return int: The id of the partner created or edited
+        """
+        values = self.values_preprocess(partner_details)
+
+        # Ensure that we won't write on unallowed fields.
+        sanitized_values = {
+            k: v for k, v in values.items() if k in self.WRITABLE_PARTNER_FIELDS
+        }
+        sanitized_custom_values = {
+            k: v for k, v in custom_values.items()
+            if k in self.WRITABLE_PARTNER_FIELDS + ['partner_id', 'parent_id', 'type']
+        }
+
+        if request.website.specific_user_account:
+            sanitized_values['website_id'] = request.website.id
+
+        lang = request.lang.code if request.lang.code in request.website.mapped(
+            'language_ids.code'
+        ) else None
+        if lang:
+            sanitized_values['lang'] = lang
+
+        partner_id = sanitized_custom_values.get('partner_id')
+        if edit and partner_id:
+            request.env['res.partner'].browse(partner_id).sudo().write(sanitized_values)
+        else:
+            sanitized_values = dict(sanitized_values, **{
+                'company_id': request.website.company_id.id,
+                'team_id': request.website.salesteam_id and request.website.salesteam_id.id,
+                'user_id': request.website.salesperson_id.id,
+                **sanitized_custom_values
+            })
+            partner_id = request.env['res.partner'].sudo().with_context(
+                tracking_disable=True
+            ).create(sanitized_values).id
+        return partner_id     
   
 class CountryInherit(models.Model):
     _inherit ="res.country"
