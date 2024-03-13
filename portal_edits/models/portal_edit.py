b7d360a12a5e30de61fc43efb878cb767088a9bf
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-03-13 11:58:44 +0300

    test

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -58,6 +58,7 @@ class WebsitePortalsInherit(WebsiteSale):
     @http.route(['/shop/address'], type='http', methods=['GET', 'POST'], auth="public", website=True, sitemap=False)
     def address(self, **kw):
         Partner = request.env['res.partner'].with_context(show_address=1).sudo()
+        delc = kw['didication_letter'] if 'didication_letter' in kw else ''
         request.env['res.partner'].sudo().browse(int(kw.get('partner_id', -1))).write({'didication_letter':kw['didication_letter'] if 'didication_letter' in kw else ''})
         
         
@@ -133,7 +134,7 @@ class WebsitePortalsInherit(WebsiteSale):
                 order.message_partner_ids = [(4, partner_id), (3, request.website.partner_id.id)]
                 if not errors:
                     return request.redirect(kw.get('callback') or '/shop/confirm_order')
-        values['didication_letter']=  ''
+        values['didication_letter']=  delc
         render_values = {
             'website_sale_order': order,
             'partner_id': partner_id,
