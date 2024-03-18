6788072a08cea8c8a8659e6e749d40e128d8fec1
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-03-18 13:02:57 +0300

    check

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -55,6 +55,55 @@ class WebsitePortalsInherit(WebsiteSale):
             #     req += ['zip']
         return req    
 
+    def checkout_form_validate(self, mode, all_form_values, data):
+        # mode: tuple ('new|edit', 'billing|shipping')
+        # all_form_values: all values before preprocess
+        # data: values after preprocess
+        error = dict()
+        error_message = []
+
+        # prevent name change if invoices exist
+        if data.get('partner_id'):
+            partner = request.env['res.partner'].browse(int(data['partner_id']))
+            if partner.exists() and partner.name and not partner.sudo().can_edit_vat() and 'name' in data and (data['name'] or False) != (partner.name or False):
+                error['name'] = 'error'
+                error_message.append(_('Changing your name is not allowed once invoices have been issued for your account. Please contact us directly for this operation.'))
+
+        # Required fields from form
+        required_fields = [f for f in (all_form_values.get('field_required') or '').split(',') if f]
+
+        # Required fields from mandatory field function
+        country_id = int(data.get('country_id', False))
+        required_fields += mode[1] == 'shipping' and self._get_mandatory_fields_shipping(country_id) or self._get_mandatory_fields_billing(country_id)
+
+        # error message for empty required fields
+        for field_name in required_fields:
+            if not data.get(field_name):
+                error[field_name] = 'missing'
+
+        # email validation
+        if data.get('email') and not tools.single_email_re.match(data.get('email')):
+            error["email"] = 'error'
+            error_message.append(_('Invalid Email! Please enter a valid email address.'))
+        if data.get('phone') and len(data.get('phone')) < 8:
+            error["phone"] = 'error'
+            error_message.append(_('Invalid number!Please enter a valid number'))
+        # vat validation
+        Partner = request.env['res.partner']
+        if data.get("vat") and hasattr(Partner, "check_vat"):
+            if country_id:
+                data["vat"] = Partner.fix_eu_vat_number(country_id, data.get("vat"))
+            partner_dummy = Partner.new(self._get_vat_validation_fields(data))
+            try:
+                partner_dummy.check_vat()
+            except ValidationError as exception:
+                error["vat"] = 'error'
+                error_message.append(exception.args[0])
+
+        if [err for err in error.values() if err == 'missing']:
+            error_message.append(_('Some required fields are empty.'))
+
+        return error, error_message
     def values_postprocess(self, order, mode, values, errors, error_msg):
         new_values = {}
         authorized_fields = request.env['ir.model']._get('res.partner')._get_form_writable_fields()
@@ -190,7 +239,7 @@ class CountryInherit(models.Model):
 
 class PartnerInherit(models.Model):
     _inherit ="res.partner"
-    didication_letter = fields.Text('Didication letter')  
+    didication_letter = fields.Text('Didication letter',website_form_blacklisted=False)  
        
 
 class AccountInherit(models.Model):    
