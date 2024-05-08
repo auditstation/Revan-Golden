73e108cd24e844369712ae546c2a8da27c2ab4f7
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-05-08 13:35:01 +0300

    check delivery

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -227,6 +227,13 @@ class WebsitePortalsInherit(WebsiteSale):
       
            order.pricelist_id = request.env['product.pricelist'].sudo().search([('currency_id','=',order.partner_shipping_id.country_id.currency_id.id)]).id
            order.sudo().action_update_prices()
+           for rec in order.order_line.filtered(lambda act: act.product_template_id.product_variant_id == 'service')
+                res = rec.order_id.carrier_id.rate_shipment()
+                rec.order_id.set_delivery_line(rec.product_template_id.product_variant_id,res['price'])
+        self.order_id.write({
+            'recompute_delivery_price': False,
+            'delivery_message': self.delivery_message,
+        })
 
         redirection = self.checkout_redirection(order) or self.checkout_check_address(order)
         if redirection:
