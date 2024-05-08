34f2a94a60ce13343dccb54de7dbd7f88b327e70
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-05-08 11:52:11 +0300

    new

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -319,8 +319,8 @@ class InheritLogin(AuthSignupHome):
                 _logger.info(f'dasasdasdasd{passwd,user.tel_pass}')
                 user.tel_pass = passwd
                 _logger.info(f'aaaaaaaaq{user.tel_pass}')
-                kw['password'] = user.tel_pass
-                request.params["password"] = user.tel_pass
+                kw['password'] = passwd
+                request.params["password"] = passwd
             elif not user.share:
                 
                
