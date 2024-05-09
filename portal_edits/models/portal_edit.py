a566fbc08f1535de28f3a8078fc5c8e280d22da4
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-05-09 15:47:07 +0300

    test

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -236,7 +236,8 @@ class WebsitePortalsInherit(WebsiteSale):
                     res = rec.order_id.carrier_id.rate_shipment(rec.order_id)
                     care= request.env['delivery.carrier'].sudo().search([('product_id','=',rec.product_template_id.product_variant_id.id)])
                     rec.order_id.set_delivery_line(care,res['price'])
-                
+                else:
+                    rec.unlink()
         redirection = self.checkout_redirection(order) or self.checkout_check_address(order)
         if redirection:
             return redirection
@@ -266,7 +267,8 @@ class WebsitePortalsInherit(WebsiteSale):
                     res = rec.order_id.carrier_id.rate_shipment(rec.order_id)
                     care= request.env['delivery.carrier'].sudo().search([('product_id','=',rec.product_template_id.product_variant_id.id)])
                     rec.order_id.set_delivery_line(care,res['price'])
-                
+                else:
+                    rec.unlink()
         redirection = self.checkout_redirection(order) or self.checkout_check_address(order)
         if redirection:
             return redirection
