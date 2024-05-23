928104356c9d86d6f9f9f44c3af3fc5228c81330
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-05-23 15:47:36 +0300

    check

--- integration_with_dalilee/models/models.py
+++ integration_with_dalilee/models/models.py
@@ -203,10 +203,9 @@ class SaleOrederInherit(models.Model):
 
     def order_status(self):
         _logger.info(f'ccccccccccccccccc')
-        for rec in self.env['sale.order'].sudo().search([('state','=', 'sale')]):
-            # .filtered(
-            #         lambda l: l.create_date.date() >= date.today()
-            #                   and l.create_date.date() <= date.today()):
+        for rec in self.env['sale.order'].sudo().search([('state','=', 'sale')]).filtered(
+                    lambda l: l.create_date.date() >= date.today()
+                              and l.create_date.date() <= date.today()):
             _logger.info(f'nnnnnnnnnnnnnnn{date.today()}')
             if rec.order_line.filtered(lambda l: l.price_total == 0):
                  rec.status_order=self.sudo().get_key_for_gov('Not')
