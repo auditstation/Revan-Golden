5ce4a9a083e760bb11fa01878043ae5b3562f2f3
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-06-02 16:59:23 +0300

    version

--- thawani_payment_gateway/models/payment_transaction.py
+++ thawani_payment_gateway/models/payment_transaction.py
@@ -85,7 +85,7 @@ class PaymentTransaction(models.Model):
                     dic ={
                     'name': rec.product_id.name,
                     'quantity': int(rec.product_uom_qty),
-                    'unit_amount': int(new_price) - int(((rec.price_unit * 1000)* dis )/100) if rec.currency_id.name =='OMR' else
+                    'unit_amount': int(new_price) - int(((new_price)* dis )/100) if rec.currency_id.name =='OMR' else
                     int((new_price * 1000 * 1.25)/sorted([i.company_rate for i in rec.currency_id.rate_ids])[-1]) - int(((rec.price_unit * 1000*1.25)* dis )/100)  if rec.currency_id.name =='KWD' else int((new_price)/sorted(rec.currency_id.rate_ids)[-1].company_rate) - int(((rec.price_unit * 1000)* dis )/100),
                     }
 
