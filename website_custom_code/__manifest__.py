79b0eb11327672e9837b07b7c64f54fbc5d05e60
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-08 17:42:59 +0200

    update

--- website_custom_code/__manifest__.py
+++ website_custom_code/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website_sale','stock'],
+    'depends': ['website','website_sale','stock'],
 
     # always loaded
     'data': [
