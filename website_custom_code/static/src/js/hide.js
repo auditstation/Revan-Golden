376196632f863694c5c33d3c04a993666c496cf8
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-09 01:31:58 +0200

    Update hide.js

--- website_custom_code/static/src/js/hide.js
+++ website_custom_code/static/src/js/hide.js
@@ -1,9 +1,12 @@
 odoo.define('hide_unavailable_variants', function (require) {
     'use strict';
-    require("website_sale.website_sale")
+
+    // Define dependencies as an array
     var ajax = require('web.ajax');
     var publicWidget = require('web.public.widget');
-    var id_tuples = undefined
+    require("website_sale.website_sale"); // This dependency does not need to be assigned to a variable
+
+    var id_tuples = undefined;
 
     publicWidget.registry.WebsiteSale.include({
 
@@ -12,34 +15,33 @@ odoo.define('hide_unavailable_variants', function (require) {
             const _super = this._super.apply(this, arguments);
 
             var $parent = $('.js_product');
-            var product_tmpl_id = parseInt($parent.find('.product_template_id').val())
+            var product_tmpl_id = parseInt($parent.find('.product_template_id').val());
             if (product_tmpl_id) {
                 proms = ajax.jsonRpc(this._getUri('/get_product_variant_data'), 'call', {
                     'product_tmpl_id': product_tmpl_id,
                 }).then((data) => {
-                    id_tuples = data
+                    id_tuples = data;
                 });
-
             }
+
             return Promise.all([this._super(...arguments), proms]);
         },
 
         onChangeVariant: function (ev) {
             const instance = this;
-
             const $parent = $(ev.target).closest('.js_product');
-            const $target = $(ev.target)
+            const $target = $(ev.target);
 
             if (!$parent.length || !id_tuples) {
                 return Promise.resolve();
             }
 
             if ($target.is('input[type=radio]') && $target.is(':checked')) {
-                instance._hideVariants($target, $parent)
+                instance._hideVariants($target, $parent);
             } else {
                 $target.find("input:checked")
                     .each(function (index) {
-                        instance._hideVariants($(this), $parent)
+                        instance._hideVariants($(this), $parent);
                     });
             }
 
@@ -49,15 +51,15 @@ odoo.define('hide_unavailable_variants', function (require) {
         _hideVariants($target, $parent) {
 
             const $variantContainer = $target.closest('ul').closest('li');
-            const currentSelect = $variantContainer.attr('data-attribute_name')
+            const currentSelect = $variantContainer.attr('data-attribute_name');
 
             if (currentSelect === 'SIZE') return;
 
             $parent.find(`li[data-attribute_name!='${currentSelect}'][data-attribute_display_type='radio']`)
                 .each(function (index) {
-                    var $current = $(this)
-                    var firstShowed = null
-                    var anyChecked = false
+                    var $current = $(this);
+                    var firstShowed = null;
+                    var anyChecked = false;
 
                     $current.find("input[type=radio]")
                         .each(function (index) {
@@ -69,25 +71,24 @@ odoo.define('hide_unavailable_variants', function (require) {
                                     const t1 = JSON.stringify([parseInt($target.val()), parseInt(input.val())]);
                                     const t2 = JSON.stringify([parseInt(input.val()), parseInt($target.val())]);
 
-                                    return tupla === t1 || tupla === t2
+                                    return tupla === t1 || tupla === t2;
                                 });
                             if (!found) {
-                                input.parent().hide()
+                                input.parent().hide();
                                 input.prop("checked", false);
                             } else {
                                 input.parent().show();
                                 if (firstShowed == null)
-                                    firstShowed = input
+                                    firstShowed = input;
 
                                 if (!anyChecked)
-                                    anyChecked = input.is(":checked")
+                                    anyChecked = input.is(":checked");
                             }
                         });
 
                     if (!anyChecked)
                         firstShowed.prop("checked", true);
                 });
-
         }
     });
 });
