e918d7c09a36baec4b4795fad201b946f3000970
noureldinbayoumi <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-12 23:24:19 +0200

    Update hide.js

--- website_custom_code/static/src/js/hide.js
+++ website_custom_code/static/src/js/hide.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 import ajax from 'web.ajax';
-import publicWidget from 'web.public.widget';
+import publicWidget from "@web/legacy/js/public/public_widget";
 import { registry } from '@web/core/registry';
 
 let id_tuples = undefined;
