b6ae87cc06701a0eab01aafef646364e99a50da5
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-12 23:47:23 +0200

    Update hide.js

--- website_custom_code/static/src/js/hide.js
+++ website_custom_code/static/src/js/hide.js
@@ -1,7 +1,11 @@
 /** @odoo-module **/
 //import ajax from 'web.ajax';
+
+
+
+
+import ajax from '@web/core/ajax';
 import publicWidget from "@web/legacy/js/public/public_widget";
-import { registry } from '@web/core/registry';
 
 let id_tuples = undefined;
 
@@ -14,9 +18,7 @@ publicWidget.registry.WebsiteSale.include({
         const $parent = $('.js_product');
         const product_tmpl_id = parseInt($parent.find('.product_template_id').val());
         if (product_tmpl_id) {
-               $.ajax({
-                    type: "POST",
-                    dataType: 'json', url:'/get_product_variant_data', 'call', {
+            proms = ajax.jsonRpc('/get_product_variant_data', 'call', {
                 'product_tmpl_id': product_tmpl_id,
             }).then((data) => {
                 id_tuples = data;
@@ -89,6 +91,101 @@ publicWidget.registry.WebsiteSale.include({
             });
     },
 });
+
+
+
+
+
+
+//import publicWidget from "@web/legacy/js/public/public_widget";
+//import { registry } from '@web/core/registry';
+//
+//let id_tuples = undefined;
+//
+//publicWidget.registry.WebsiteSale.include({
+//
+//    async willStart() {
+//        await this._super.apply(this, arguments);
+//
+//        let proms;
+//        const $parent = $('.js_product');
+//        const product_tmpl_id = parseInt($parent.find('.product_template_id').val());
+//        if (product_tmpl_id) {
+//               $.ajax({
+//                    type: "POST",
+//                    dataType: 'json', url:'/get_product_variant_data', 'call', {
+//                'product_tmpl_id': product_tmpl_id,
+//            }).then((data) => {
+//                id_tuples = data;
+//            });
+//        }
+//        await Promise.all([proms]);
+//    },
+//
+//    onChangeVariant(ev) {
+//        const instance = this;
+//        const $parent = $(ev.target).closest('.js_product');
+//        const $target = $(ev.target);
+//
+//        if (!$parent.length || !id_tuples) {
+//            return Promise.resolve();
+//        }
+//
+//        if ($target.is('input[type=radio]') && $target.is(':checked')) {
+//            instance._hideVariants($target, $parent);
+//        } else {
+//            $target.find("input:checked")
+//                .each(function () {
+//                    instance._hideVariants($(this), $parent);
+//                });
+//        }
+//
+//        return this._super.apply(this, arguments);
+//    },
+//
+//    _hideVariants($target, $parent) {
+//        const $variantContainer = $target.closest('ul').closest('li');
+//        const currentSelect = $variantContainer.attr('data-attribute_name');
+//
+//        if (currentSelect === 'SIZE') return;
+//
+//        $parent.find(`li[data-attribute_name!='${currentSelect}'][data-attribute_display_type='radio']`)
+//            .each(function () {
+//                const $current = $(this);
+//                let firstShowed = null;
+//                let anyChecked = false;
+//
+//                $current.find("input[type=radio]")
+//                    .each(function () {
+//                        const input = $(this);
+//                        const found = id_tuples.value_to_show_tuple
+//                            .find(function (el) {
+//                                const tupla = JSON.stringify(el);
+//                                const t1 = JSON.stringify([parseInt($target.val()), parseInt(input.val())]);
+//                                const t2 = JSON.stringify([parseInt(input.val()), parseInt($target.val())]);
+//
+//                                return tupla === t1 || tupla === t2;
+//                            });
+//                        if (!found) {
+//                            input.parent().hide();
+//                            input.prop("checked", false);
+//                        } else {
+//                            input.parent().show();
+//                            if (firstShowed == null) {
+//                                firstShowed = input;
+//                            }
+//                            if (!anyChecked) {
+//                                anyChecked = input.is(":checked");
+//                            }
+//                        }
+//                    });
+//
+//                if (!anyChecked && firstShowed) {
+//                    firstShowed.prop("checked", true);
+//                }
+//            });
+//    },
+//});
 
 
 
