70daf635e6d4dfc60699baa40af532b87f306305
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-13 11:16:14 +0200

    Update controllers.py

--- website_custom_code/controllers/controllers.py
+++ website_custom_code/controllers/controllers.py
@@ -15,33 +15,22 @@ from odoo import http
 from odoo.http import request
 
 
-# class HideVariant(http.Controller):
+class HideVariant(http.Controller):
 #     @http.route("/get_product_variant_data_website", type="json", website=True, auth="public", methods=["POST"])
 #     def get_product_variant_data(self, product_tmpl_id):
-#
-#         product_tmpl_id = request.env["product.template"].search([("id", "=", int(product_tmpl_id))])
-#         if product_tmpl_id:
-#             return product_tmpl_id.get_possible_combinations_available()
-#         if not product_tmpl_id:
-#             return {
-#                 'error': True,
-#                 'message': 'Product Template ID is missing'
-#             }
-
-from odoo import http
-
-
-class HideVariant(http.Controller):
     @http.route('/get_product_variant_data_website', type='json', auth='public', methods=['POST'], csrf=False)
     def get_product_variant_data(self):
-        # Access the JSON data sent in the request
+    # Access the JSON data sent in the request
         product_tmpl_id = http.request.jsonrequest.get('product_tmpl_id')
 
+        product_tmpl_id = request.env["product.template"].search([("id", "=", int(product_tmpl_id))])
+        if product_tmpl_id:
+            return product_tmpl_id.get_possible_combinations_available()
         if not product_tmpl_id:
-            return {'error': True, 'message': 'Missing product_tmpl_id'}
-
-        # Your logic to fetch the product variant data
-        # ...
+            return {
+                'error': True,
+                'message': 'Product Template ID is missingsss'
+            }
         # try:
         #     # Convert the ID to an integer and fetch the product template
         #     product_tmpl_id = int(product_tmpl_id)
