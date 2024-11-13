68eed98eb8adc3192e8beff821c4f9505dd7ec47
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-13 12:26:12 +0200

    Update hide.js

--- website_custom_code/static/src/js/hide.js
+++ website_custom_code/static/src/js/hide.js
@@ -34,6 +34,8 @@ publicWidget.registry.WebsiteSale.include({
                 // Check if the response contains the expected data
                 if (response) {
                     id_tuples = response.value_to_show_tuple;
+                    console.log("id_tuplessssss>>>",id_tuples)
+
                 } else {
                     console.error("Invalid data structure returned:", response);
                 }
