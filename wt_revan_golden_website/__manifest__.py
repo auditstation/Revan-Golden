edbaf1ff17d0a1930dfe9f31d2a30ebcb49eba08
noureldinbayoumi <58402085+noureldinbayomie@users.noreply.github.com>
2024-11-19 10:03:51 +0200

    Update __manifest__.py

--- wt_revan_golden_website/__manifest__.py
+++ wt_revan_golden_website/__manifest__.py
@@ -27,7 +27,7 @@
     'depends': ['website','base','website_sale','portal','website_custom_code','website_sale_wishlist'],
     'data': [
         "views/homepage.xml",
-        "views/portal_my_home.xml",
+        #"views/portal_my_home.xml",
         "views/contactus.xml",
         "views/website_product.xml",
         "views/website_sale.xml",
