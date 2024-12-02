642f7137459781fb41d23d0b31c1f4d50222d44b
tech-devlp <160579593+tech-devlp@users.noreply.github.com>
2024-12-02 14:27:14 +0300

    Update portal_edit.py

--- portal_edits/models/portal_edit.py
+++ portal_edits/models/portal_edit.py
@@ -124,7 +124,7 @@ class WebsitePortalsInherit(WebsiteSale):
             elif  data_phone[0:4] == "+"+prefix_code and data_phone[4:]!=phone_limit:
                 error["phone"] = 'error'
                 error_message.append(_('Invalid number! Please enter a valid number with limit %s and country code',str(phone_limit)))
-            elif data_phone[0:2] =='00' and data_phone[2:5] == prefix_code and len((data_phone[5:]))!=phone_limit): 
+            elif data_phone[0:2] =='00' and data_phone[2:5] == prefix_code and len((data_phone[5:]))!=phone_limit: 
                 error["phone"] = 'error'
                 error_message.append(_('Invalid number! Please enter a valid number with limit %s and country code',str(phone_limit)))
         return error, error_message
