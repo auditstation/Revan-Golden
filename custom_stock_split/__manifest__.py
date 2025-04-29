f7626b448b567055370b7b6f6ef55361b0cd3cf2
noureldinbayomie <58402085+noureldinbayomie@users.noreply.github.com>
2025-04-29 23:13:28 +0300

    Update __manifest__.py

--- custom_stock_split/__manifest__.py
+++ custom_stock_split/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Stock Partial Pull Split",
     "version": "1.0",
-    "depends": ['stock','base','sale_management',],
+    "depends": ['stock','base','sale_management','website_sale'],
     "category": "Inventory",
     "description": "Automatically split stock moves between khoud and bawshar if quantity is insufficient.",
     "data": [],
