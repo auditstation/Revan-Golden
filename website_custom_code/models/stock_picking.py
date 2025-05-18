a5028b83e2b3a468830958294b9df6312ad61b90
noureldinbayomie <46438670+HassanYousef2@users.noreply.github.com>
2025-05-18 23:38:54 +0300

    Update stock_picking.py

--- website_custom_code/models/stock_picking.py
+++ website_custom_code/models/stock_picking.py
@@ -3,20 +3,45 @@ from odoo import models
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    def button_validate(self):
-        res = super(StockPicking,self).button_validate()
+    # def button_validate(self):
+    #     res = super(StockPicking,self).button_validate()
+    #
+    #     for picking in self:
+    #         if picking.picking_type_id.code == 'internal' and picking.sale_id:
+    #             delivery_pickings = self.env['stock.picking'].search([
+    #                 ('sale_id', '=', picking.sale_id.id),
+    #                 ('picking_type_id.code', '=', 'outgoing'),
+    #             ])
+    #             if delivery_pickings:
+    #                 for delivery_picking in delivery_pickings:
+    #                     for move in delivery_picking.move_ids_without_package:
+    #                         move.quantity  = move.product_uom_qty
+    #                     delivery_picking.action_assign()
+    #                     delivery_picking.button_validate()
+    #
+    #     return res
+    #
+    #
 
+    def button_validate(self):
         for picking in self:
+            # If it's an internal picking linked to a sale
             if picking.picking_type_id.code == 'internal' and picking.sale_id:
+                # First handle outgoing delivery
                 delivery_pickings = self.env['stock.picking'].search([
                     ('sale_id', '=', picking.sale_id.id),
                     ('picking_type_id.code', '=', 'outgoing'),
+                    ('state', 'not in', ['done', 'cancel']),
                 ])
-                if delivery_pickings:
-                    for delivery_picking in delivery_pickings:
-                        for move in delivery_picking.move_ids_without_package:
-                            move.quantity  = move.product_uom_qty
-                        delivery_picking.action_assign()
+                for delivery_picking in delivery_pickings:
+                    for move in delivery_picking.move_ids_without_package:
+                        move.quantity = move.product_uom_qty
+                    delivery_picking.action_assign()
+                    if delivery_picking._check_quantity():
                         delivery_picking.button_validate()
-
-        return res
+                    else:
+                        raise UserError(
+                            f"Cannot validate delivery for '{delivery_picking.name}' due to insufficient stock."
+                        )
+        # Now proceed with the original validation
+        return super(StockPicking, self).button_validate()
