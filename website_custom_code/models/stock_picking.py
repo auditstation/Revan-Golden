d7c520b4b6f16081d7e9e61a9b34d0328ece3c1b
noureldinbayomie <46438670+HassanYousef2@users.noreply.github.com>
2025-05-18 23:59:15 +0300

    Update stock_picking.py

--- website_custom_code/models/stock_picking.py
+++ website_custom_code/models/stock_picking.py
@@ -23,10 +23,10 @@ class StockPicking(models.Model):
                         # Only process if there are stock moves to work with
                         if delivery_picking.move_ids_without_package:
                             for move in delivery_picking.move_ids_without_package:
-                                move.quantity_done = move.product_uom_qty
+                                move.quantity = move.product_uom_qty
 
                             # Assign stock and validate
-                            delivery_picking.action_assign()
+                            # delivery_picking.action_assign()
                             delivery_picking.button_validate()
 
         return res
